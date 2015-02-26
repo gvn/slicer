3a3d900db17e4600153801c619c03495a17374f1
Gavin Lazar Suntop <gavin@gsuntop.com>
2015-02-25 19:05:19 -0800

    lots of cleanup

--- _ui/js/slicer.js
+++ _ui/js/slicer.js
@@ -1,59 +1,36 @@
-$(document).ready(function() {
+var elImg = $('img');
 
-	var elImg = $('img');
-	var elImgHeight = elImg.height();
-	var elImgWidth = elImg.width();
+var elImgHeight = elImg.height();
+var elImgWidth = elImg.width();
 
-	elImg.css({'margin-left':elImgWidth/-2});
-	
-	var elSlices = $('<ul class="slices"></ul>').css({width:elImgWidth,margin:'auto'});
-	
-	var sliceHeight = 50;
-	var sliceCount = Math.floor(elImgHeight / sliceHeight);
+elImg.css({'margin-left': elImgWidth / -2});
 
-	$('body').append(elSlices);
-	
-	for(i=0;i<sliceCount;i++) {
-		var elSlice = $('<li></li>');
-		
-		var offset;
-		Math.random() > 0.5 ? offset = 0 : offset = 1;
-		
-		// SETUP SLICES
-		elSlice.css({
-			height:sliceHeight+Math.random()*10,
-			width:elImgWidth,
-			background:'url('+elImg[0].src+') 0 -'+(i)*(sliceHeight+i)+'px repeat-y'
-		});
-		
-		elSlices.append(elSlice);
-		
-		// FILTER
-		var direction = Math.random();
-		direction > 0.5 ? direction = -1 : direction = 1;
-		
-		elSlice.css({
-			'-webkit-transform':'skew(-'+Math.random()*10+'deg) scale('+(0.9+Math.random()*0.2)+') rotate('+Math.random()*2*direction+'deg)',
-			opacity:Math.random()+0.5
-		});
-	}
-	
-	
-	
-	
-	function budge() {
-		$('ul.slices li').each(function(){
-			var direction = Math.random();
-			direction > 0.5 ? direction = -1 : direction = 1;
-			
-			$(this).css({
-				'-webkit-transform':'skew(-'+Math.random()*10*direction+'deg) scale('+(0.9+Math.random()*0.2)+') rotate('+Math.random()*2*direction+'deg)',
-				opacity:Math.random()+0.5
-			});
-		});
-	}
-	
-	// budge();
-	// setInterval(budge,3000);
+var elSlices = $('<ul class="slices"></ul>').css({width:elImgWidth, margin:'auto'});
 
-});
+var sliceCount = 10;
+var sliceHeight = Math.floor(elImgHeight / sliceCount);
+
+$('body').append(elSlices);
+
+for(i=0; i<sliceCount; i++) {
+  var elSlice = $('<li></li>');
+
+  var offset = Math.random() > 0.5 ? 0 : 1;
+
+  // SETUP SLICES
+  elSlice.css({
+    height: sliceHeight, //+Math.random()*10
+    width: elImgWidth,
+    background: 'url(' + elImg[0].src + ') 0 -' + (i * sliceHeight) + 'px repeat-y'
+  });
+
+  elSlices.append(elSlice);
+
+  // FILTER
+  var direction = Math.random() > 0.5 ? -1 : 1;
+
+  elSlice.css({
+    '-webkit-transform': 'skew(-' + Math.random() * 10 + 'deg) scale(' + (0.9 + Math.random() *0.2) + ') rotate(' + Math.random() * 2 * direction + 'deg)',
+    opacity: Math.random()
+  });
+}
