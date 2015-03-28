5d728eb133043caab52155e4b2d2921f7fda74b4
Gavin Lazar Suntop <gavin@gsuntop.com>
2015-03-28 14:30:03 -0700

    enabling source and destination in CLI

--- _ui/js/slicer.js
+++ _ui/js/slicer.js
@@ -1,63 +1,69 @@
-const BLUR = true;
-const BLUR_MAX = 2;
-const SLICE_COUNT = 10;
-const SKEW_MAX = 10;
-const ROTATE_MAX = 2;
-const ADD_BORDER = true;
-const BORDER_COLOR = 'rgba(69,69,69,0.2)';
-const ALLOW_REPEATS = false;
-const REPEAT_PROBABILITY = 0.2; // float: 0 (never) -> 1 (always)
+// Example in-browser usage:
+// $('img').attr('src', '_ui/img/wetcat-futura.png');
+// slice();
 
-var elImg = $('img');
+function slice () {
+  const BLUR = true;
+  const BLUR_MAX = 2;
+  const SLICE_COUNT = 10;
+  const SKEW_MAX = 10;
+  const ROTATE_MAX = 2;
+  const ADD_BORDER = true;
+  const BORDER_COLOR = 'rgba(69,69,69,0.2)';
+  const ALLOW_REPEATS = false;
+  const REPEAT_PROBABILITY = 0.2; // float: 0 (never) -> 1 (always)
 
-var imgHeight = elImg.height();
-var imgWidth = elImg.width();
+  var elImg = $('img');
 
-var elSlices = $('<ul class="slices"></ul>').css({width: imgWidth});
-var sliceHeight = Math.floor(imgHeight / SLICE_COUNT);
+  var imgHeight = elImg.height();
+  var imgWidth = elImg.width();
 
-$('body').append(elSlices);
+  var elSlices = $('<ul class="slices"></ul>').css({width: imgWidth});
+  var sliceHeight = Math.floor(imgHeight / SLICE_COUNT);
 
-// Create slices: -------------------------------------------------------------
+  $('body').append(elSlices);
 
-for(i = 0; i < SLICE_COUNT; i++) {
-  var elSlice = $('<li></li>');
-  var sliceToShow = i;
+  // Create slices: -------------------------------------------------------------
 
-  if (ALLOW_REPEATS) {
-    sliceToShow = Math.random() > REPEAT_PROBABILITY ? i : i - 1;
-  }
+  for(i = 0; i < SLICE_COUNT; i++) {
+    var elSlice = $('<li></li>');
+    var sliceToShow = i;
 
-  elSlice.css({
-    height: sliceHeight,
-    width: imgWidth,
-    background: 'url(' + elImg[0].src + ') 0 -' + (sliceToShow * sliceHeight) + 'px repeat-y'
-  });
+    if (ALLOW_REPEATS) {
+      sliceToShow = Math.random() > REPEAT_PROBABILITY ? i : i - 1;
+    }
 
-  // Jitter slices:
-
-  var direction = Math.random() > 0.5 ? -1 : 1;
-
-  elSlice.css({
-    '-webkit-transform':
-      'skewX(-' + (direction * Math.random() * SKEW_MAX) + 'deg) ' +
-      'scale(' + (0.9 + Math.random() * 0.2) + ') ' +
-      'rotate(' + (direction * Math.random() * ROTATE_MAX) + 'deg)',
-    opacity: Math.random()
-  });
-
-  if (BLUR) {
     elSlice.css({
-      '-webkit-filter': 'blur(' + Math.floor(Math.random() * BLUR_MAX) + 'px)'
+      height: sliceHeight,
+      width: imgWidth,
+      background: 'url(' + elImg[0].src + ') 0 -' + (sliceToShow * sliceHeight) + 'px repeat-y'
     });
-  }
 
-  if (ADD_BORDER) {
+    // Jitter slices:
+
+    var direction = Math.random() > 0.5 ? -1 : 1;
+
     elSlice.css({
-      'border-top': '1px solid ' + BORDER_COLOR,
-      'border-bottom': '1px solid ' + BORDER_COLOR
+      '-webkit-transform':
+        'skewX(-' + (direction * Math.random() * SKEW_MAX) + 'deg) ' +
+        'scale(' + (0.9 + Math.random() * 0.2) + ') ' +
+        'rotate(' + (direction * Math.random() * ROTATE_MAX) + 'deg)',
+      opacity: Math.random()
     });
-  }
 
-  elSlices.append(elSlice);
+    if (BLUR) {
+      elSlice.css({
+        '-webkit-filter': 'blur(' + Math.floor(Math.random() * BLUR_MAX) + 'px)'
+      });
+    }
+
+    if (ADD_BORDER) {
+      elSlice.css({
+        'border-top': '1px solid ' + BORDER_COLOR,
+        'border-bottom': '1px solid ' + BORDER_COLOR
+      });
+    }
+
+    elSlices.append(elSlice);
+  }
 }
--- render.js
+++ render.js
@@ -1,15 +1,43 @@
+// USAGE: phantomjs render.js INPUT_IMAGE OUTPUT_IMAGE
+
+var system = require('system');
+var args = system.args;
 var page = require('webpage').create();
 
+var imgInput = args[1];
+var imgOutput = args[2];
+
 page.open('http://localhost:31337', function (status) {
-  var viewportSize = page.evaluate(function () {
-    return {
-      width: document.querySelector('img').width,
-      height: document.querySelector('img').height
+  // Inject image into DOM
+  page.evaluate(function (imagePath) {
+    $('img').attr('src', imagePath);
+  }, imgInput);
+
+  // Ensure image has dimensions before slicing
+  var imageReadyCheck = setInterval(function (arguments) {
+    var imgWidth = page.evaluate(function () {
+      return $('img').width()
+    });
+
+    if (imgWidth) {
+      clearInterval(imageReadyCheck);
+      sliceAndRender();
     }
-  });
+  }, 1);
 
-  page.viewportSize = viewportSize;
-  page.render('rendered/test.png');
+  function sliceAndRender () {
+    page.viewportSize = page.evaluate(function () {
+      return {
+        width: document.querySelector('img').width,
+        height: document.querySelector('img').height
+      }
+    });
 
-  phantom.exit();
+    page.evaluate(function () {
+      slice();
+    });
+
+    page.render(imgOutput);
+    phantom.exit();
+  }
 });
